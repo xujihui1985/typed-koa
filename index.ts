728180c6c329f9834db055480be7346a6f0f2a58
Sean <xujihui1985@gmail.com>
2016-12-18 21:14:49 +0800

    update Startup

--- index.ts
+++ index.ts
@@ -1,12 +1,34 @@
 import * as http from 'http';
 import * as Koa from 'koa';
+import { Startup } from './Startup';
 import admin from './areas/admin';
 
+class AppBuilder {
+  use() {
+    return this;
+  }
+
+  build(app: Koa) {
+    
+  }
+
+}
+
+class HostingEnviroment {
+  isDevelopment(): boolean {
+    return true;
+  }
+}
+const appBuilder = new AppBuilder();
+const env = new HostingEnviroment();
+
+const startup = new Startup();
+startup.configure(appBuilder, env);
+
 const app = new Koa();
+
+appBuilder.build(app);
 app.use(admin.routes());
-app.onerror(err => {
-  console.log('Error: ', err);
-});
 
 http
   .createServer(app.callback())
